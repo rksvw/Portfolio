e6a94fdeb8753c5ad1dcfb238a79e6d4144bc1ba
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-13 23:57:04 +0530

    Installed react-router-dom to routes pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
-
   return (
     <>
-      <div>Hello world!</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
