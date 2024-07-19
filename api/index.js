ac47311be95ca525ba1db0681a4eb891620643d0
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-19 19:10:25 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const userRoutes = require("./routes.js");
+const bodyParser = require("body-parser");
 
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.get('/', (req, res) => {
-    res.send("Hello world!");
-})
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
+// app.use("/api/v1", userRoutes);
+// app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-})
+app.listen(3500, () => {
+  console.log("Server is listening on port 3000");
+});
