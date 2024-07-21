2743b81a3e6029f05b3165e367e2ff102763eb36
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-21 11:09:07 +0530

    change in header ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,63 +1,46 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
 
 function Header() {
   const path = useLocation().pathname;
 
   return (
-    <Navbar className="border-b-2 border-black bg-[rgba(42,42,42,0.9)]">
-      <div className="w-[10%] flex items-center justify-start">
-        <Link
-          to="/"
-          className="self-center text-sm font-semibold md:text-xl whitespace-nowrap"
-        >
-          <span className="px-2 py-1 bg-gradient-to-tr from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-lg text-white">
-            Egist
+    <Navbar className="flex justify-between items-center border-b fixed top-0 left-0 w-full">
+      {/* left */}
+      <div className="flex">
+        <Link to={"/"}>
+          <span className="px-2 py-1 bg-gradient-to-tr from-lime-400 via-indigo-700 to-rose-300 rounded-lg text-white">
+            Cyrotik
           </span>
-          folio
         </Link>
       </div>
-      <form className="w-[34%] flex justify-start items-center">
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline w-96 text-[#4f4f4f] font-semibold"
-        />
-      </form>
-      <div className="flex justify-between items-center w-[30%]">
+      {/* middle */}
+
+      <div className="flex gap-4 justify-center items-center">
         <Navbar.Collapse>
           <Navbar.Link action={path === "/"} as={"div"}>
-            <Link to="/" className="text-stone-200">
-              Home
-            </Link>
+            <Link to={"/"}>Recent</Link>
+          </Navbar.Link>
+          <Navbar.Link action={path === "/anime"} as={"div"}>
+            <Link to={"/anime"}>Anime</Link>
           </Navbar.Link>
           <Navbar.Link action={path === "/about"} as={"div"}>
-            <Link to="/about" className="text-stone-200">
-              About
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link action={path === "/projects"} as={"div"}>
-            <Link to="/filter" className="text-stone-200">
-              Filter
-            </Link>
+            <Link to={"/about"}>About</Link>
           </Navbar.Link>
         </Navbar.Collapse>
-        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-          <AiOutlineSearch />
-        </Button>
-        <div className="flex gap-2 md:order-2">
-          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-            <FaMoon />
-          </Button>
-          <Link to="sign-in">
-            <Button gradientDuoTone={"purpleToBlue"}>Sign In</Button>
-          </Link>
-          <Navbar.Toggle />
-        </div>
       </div>
+      {/* right */}
+      <div className="flex">
+        <form className="w-96">
+          <TextInput
+            className="border-gray-300 focus:border-indigo-500 rounded-lg px-4 py-2"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+          />
+        </form>
+      </div>
+      <Navbar.Toggle />
     </Navbar>
   );
 }
