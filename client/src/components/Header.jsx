0135bb1351f9a0f268bc973af299cb2ff43918e6
Ritik Sharma <156570972+rksvw@users.noreply.github.com>
2024-07-22 14:59:11 +0530

    updated header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import { Navbar, TextInput } from "flowbite-react";
+import { Navbar, TextInput, Dropdown, Avatar, Button } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
     <Navbar className="flex justify-between items-center border-b fixed top-0 left-0 w-full z-10">
@@ -16,6 +18,16 @@ function Header() {
         </Link>
       </div>
       {/* middle */}
+      <div className="flex">
+        <form className="w-96">
+          <TextInput
+            className="border-gray-300 focus:border-indigo-500 rounded-lg px-4 py-2"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+          />
+        </form>
+      </div>
+      {/* left */}
 
       <div className="flex gap-4 justify-center items-center">
         <Navbar.Collapse>
@@ -30,17 +42,32 @@ function Header() {
           </Navbar.Link>
         </Navbar.Collapse>
       </div>
-      {/* right */}
-      <div className="flex">
-        <form className="w-96">
-          <TextInput
-            className="border-gray-300 focus:border-indigo-500 rounded-lg px-4 py-2"
-            placeholder="Search..."
-            rightIcon={AiOutlineSearch}
-          />
-        </form>
-      </div>
       <Navbar.Toggle />
+      {currentUser ? (
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={<Avatar alt="user" img={currentUser.profilePicture} rounded />}
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">@{currentUser.username}</span>
+            <span className="block text-sm font-medium truncate">
+              {currentUser.email}
+            </span>
+          </Dropdown.Header>
+          <Link to={"/dashboard?tab=profile"}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign Out</Dropdown.Item>
+        </Dropdown>
+      ) : (
+        <Link to={"/sign-in"}>
+          <Button gradientDuoTone={"purpleToBlue"} outline>
+            Sign In
+          </Button>
+        </Link>
+      )}
     </Navbar>
   );
 }
